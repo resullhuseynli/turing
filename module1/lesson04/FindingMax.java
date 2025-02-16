8a95685cf3d769e3bc3dc86e382988a0ab6fb91c
Resul Huseynli <resulhuseynli4@gmail.com>
2025-02-16 19:32:52 +0400

    FindigMax fixed!

--- module1/lesson04/FindingMax.java
+++ module1/lesson04/FindingMax.java
@@ -1,15 +1,22 @@
 package module1.lesson04;
 
+import java.util.Scanner;
+
 public class FindingMax {
 
     public static void main(String[] args) {
 
-        int[] numbers = {1,2,11,3,4,5,7,10,8,9};
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = new int[scanner.nextInt()];
+
         int result = 0;
 
-        for (int number : numbers) {
+        for (int i=0 ; i<arr.length ; i++) {
 
-            result = Math.max(result, number);
+            arr[i] = scanner.nextInt();
+
+            result = Math.max(result, arr[i]);
 
         }
 
