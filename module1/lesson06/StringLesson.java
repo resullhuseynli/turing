df19eaeb09ccdfc4ca5163f6a85f315c57bab82f
Resul Huseynli <resulhuseynli4@gmail.com>
2025-02-24 12:31:38 +0400

    String Lesson added!

---
+++ module1/lesson06/StringLesson.java
@@ -0,0 +1,15 @@
+package module1.lesson06;
+
+public class StringLesson {
+    public static void main(String[] args) {
+
+
+    String name = "Resul" ;
+    String name2 = new String("Resul");
+
+    System.out.println(name);
+
+
+    }
+
+}
