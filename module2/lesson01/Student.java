2c899aa46c2609223153f0591eb05cdc552cc9ee
Resul Huseynli <resulhuseynli4@gmail.com>
2025-03-03 23:36:18 +0400

    Student Class added!

--- module2/lesson01/Student.java
+++ module2/lesson01/Student.java
@@ -2,13 +2,48 @@ package module2.lesson01;
 
 public class Student {
 
-    public Student() {}
+    public Student() {
+        id++;
+    }
 
     public Student(String name, int age) {
         this.name = name;
         this.age = age;
+        id++;
     }
 
-    public String name;
-    public int age;
+    /* public Student(String name) {
+        this(name, 18); // This is a constructor call. It is used to call the constructor of the same class.
+    }*/
+
+    private static Long id=0L;
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Id: " + id + "\n" +
+                "Name: " + name + "\n" +
+                "Age: " + age + "\n" ;
+    }
 }
