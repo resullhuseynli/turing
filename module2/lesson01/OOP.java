b5f5ffacda247b2d3035928a79742b8de956b172
Resul Huseynli <resulhuseynli4@gmail.com>
2025-03-03 23:36:53 +0400

    OOP class Updated!

--- module2/lesson01/OOP.java
+++ module2/lesson01/OOP.java
@@ -4,11 +4,28 @@ public class OOP {
     public static void main(String[] args) {
 
         Student student = new Student();
-        student.name = "John";
-        student.age = 20;
-
-        System.out.println("Name: " + student.name);
-        System.out.println("Age: " + student.age);
+        student.setAge(19);
+        student.setName("Aydin");
         System.out.println(student);
+
+        Student student2 = new Student();
+        student2.setAge(20);
+        student2.setName("Rasul");
+        System.out.println(student2);
+
+        Student student3 = new Student("Serkan", 20);
+        System.out.println(student3);
+
+        System.out.println(addNumber(1,2));
+    }
+
+    public static int addNumber(int a, int b) {
+        int sum = a + b;
+        return a + b;
+    }
+
+    public int addNumber(int a, int b, int c) {
+        int sum = addNumber(a, b);
+        return sum + c;
     }
 }
